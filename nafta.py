2d55860b186dda1044da140817e57507d4376c56
Pia Klemenc <pia.klemenc1@gmail.com>
2022-01-14 19:38:11 +0100

    koda3

--- nafta.py
+++ nafta.py
@@ -1,36 +1,34 @@
 from email.charset import QP
+import math
 
+# d - dobiček
+# X - trenutna zaloga
+# K - max izčrpana dnevna količna
+# P - cena nafte
+# STRc - stroški črpanja nafte
+# STRs - stroški shranjevanja nafte
 
-
-
-def nafta(d, X, K, dni, P, S):
-    #(nafta(0, 0, 50, 5, 4, 200))
-    rezultati = {}
+def nafta(d, X, K, dni, P, S, STRc, STRs):
+    # npr. nafta(0, 0, 50, 5, 6, 200, [1, 2, 4, 1, 2],[1, 1, 6, 2, 3])
+    koncni_rezultati = {}
     dnevni_rezultati = {}
-    STRc=[1, 1, 1.4, 2, 2]
-    STRs=[1, 1.5, 1.4, 1, 2]
     dobicki_v_dnevu = [] 
     for i in range(dni): #za vsak dan
         if i == 0:
-            rezultati[0] = (0,0)
-        for Qi in range(K): #Koliko izčrpam<=K
-            for Qp in range(Qi+X): #koliko prodam <= koliko izčrpam+ začetna zaloga
-                while X+Qi-Qp <= S:
-                    di=P*Qp-Qi*STRc[i]-STRs[i]*(X+Qi-Qp)
-                    dnevni_rezultati[di] = (Qi, Qp) #dodajava v slovar
+            končni_rezultati[0] = (0,0)
+        for Qi in range(K): #Koliko izčrpamo<=K
+            for Qp in range(Qi+X): #koliko prodamo <= koliko izčrpam + začetna zaloga
+                if X+Qi-Qp <= S: 
+                    di=P*Qp-Qi*STRc[i]-STRs[i]*(X+Qi-Qp) #dobiček
+                    dnevni_rezultati[di] = (Qi, Qp) #dodajava v slovar dobiček in količino Qi in Qp
                     dobicki_v_dnevu += [di] #dodajava v seznam
-                    M = max(dobicki_v_dnevu)
-                    par = dnevni_rezultati[M]
+                else:
+                    pass
+        M = max(dobicki_v_dnevu)
+        par = dnevni_rezultati[M]
         d += M 
-        rezultati[M] = dnevni_rezultati.get(M)
-    
-    #od določenega key-a želiva dobiti pripadajoči
-    # value in dodati v slovar rezultati 
-    #X += Qi - Qp ... zaloga na koncu dneva
-        X += par[0] - par[1]
-
-
-
+        koncni_rezultati[M] = dnevni_rezultati.get(M)
+        X += par[0] - par[1]  #X += Qi - Qp ... zaloga na koncu dneva
         dobicki_v_dnevu = []         
         dnevni_rezultati = {}
-    return(rezultati)
+    return koncni_rezultati
