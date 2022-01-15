e63be0d27d4fa89b308ba323c02482565fa74cc1
Pia Klemenc <pia.klemenc1@gmail.com>
2022-01-15 12:27:45 +0100

    koda4

--- nafta.py
+++ nafta.py
@@ -8,27 +8,30 @@ import math
 # STRc - stroški črpanja nafte
 # STRs - stroški shranjevanja nafte
 
-def nafta(d, X, K, dni, P, S, STRc, STRs):
-    # npr. nafta(0, 0, 50, 5, 6, 200, [1, 2, 4, 1, 2],[1, 1, 6, 2, 3])
+def nafta(K, dni, P, S, STRc, STRs):
+    def nafta(K, dni, P, S, STRc, STRs):
+    # npr. nafta(10, 3, 30, 40, [5, 10, 7],[13, 14, 9])
     koncni_rezultati = {}
     dnevni_rezultati = {}
-    dobicki_v_dnevu = [] 
+    dobicki_v_dnevu = []
+    X = 0
+    d = 0
     for i in range(dni): #za vsak dan
-        if i == 0:
-            končni_rezultati[0] = (0,0)
-        for Qi in range(K): #Koliko izčrpamo<=K
-            for Qp in range(Qi+X): #koliko prodamo <= koliko izčrpam + začetna zaloga
-                if X+Qi-Qp <= S: 
-                    di=P*Qp-Qi*STRc[i]-STRs[i]*(X+Qi-Qp) #dobiček
-                    dnevni_rezultati[di] = (Qi, Qp) #dodajava v slovar dobiček in količino Qi in Qp
-                    dobicki_v_dnevu += [di] #dodajava v seznam
-                else:
-                    pass
+        for Qi in range(K+1): #Koliko izčrpamo<=K
+            for Qp in range(Qi+X+1): #koliko prodamo <= koliko izčrpam + začetna zaloga
+                for x in range(S+1): # x optimalna zaloga 
+                    if x == X +Qi-Qp: 
+                        di=P*Qp-Qi*STRc[i]-STRs[i]*x #dobiček
+                        dnevni_rezultati[di] = (Qi, Qp, x) #dodajava v slovar dobiček in količino Qi in Qp
+                        dobicki_v_dnevu += [di] #dodajava v seznam
+                    else:
+                        pass
         M = max(dobicki_v_dnevu)
         par = dnevni_rezultati[M]
+        X = x
         d += M 
         koncni_rezultati[M] = dnevni_rezultati.get(M)
-        X += par[0] - par[1]  #X += Qi - Qp ... zaloga na koncu dneva
         dobicki_v_dnevu = []         
         dnevni_rezultati = {}
     return koncni_rezultati
+
