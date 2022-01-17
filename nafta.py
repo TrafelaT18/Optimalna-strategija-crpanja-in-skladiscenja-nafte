166011d1f9628a907b1d6f78731a76aa0aef00e1
TrafelaT18 <49915070+TrafelaT18@users.noreply.github.com>
2022-01-17 13:56:03 +0100

    Update nafta.py

--- nafta.py
+++ nafta.py
@@ -10,23 +10,21 @@ def nafta(dni, K,S, P, STRc, STRs):
         for x in range(S+1): # končna zaloga dne i
             kandidati = [(-inf, (0, 0, 0))] # seznam dobičkov 
             for Qi in range(K+1): # koliko izčrpamo <= K
-                for Qp in range(max(0, Qi - x), S + Qi - x + 1):# koliko prodamo
-                    #>= višek, <= kapaciteta + višek
-                    x_vceraj = x + Qp - Qi # zaloga iz prejšnjega dne oz. začetna zaloga
+                for Qp in range(max(0, Qi - x), S + Qi - x + 1):# višek če ni začetne zaloge<=koliko prodamo<=višek če je polna začetna zaloga(kapaciteta+višek)
+                    x_vceraj = x + Qp - Qi # zaloga iz prejšnjega dne oz. začetna zaloga tega dne
                     ix_vceraj = (i, x_vceraj) # podatki za prejšnji dan
-                    if ix_vceraj in d: # preverimo, ali imamo podatke za prejšnji dan
+                    if ix_vceraj in d: # preverimo, ali imamo podatke prejšnjega dne
                         d_strategije = P[i] * Qp - STRc[i] * Qi - STRs[i] * x  #dnevni dobiček
                         di = d_strategije + d[ix_vceraj] # skupni dobiček
-                        kandidati.append((di, (d_strategije, Qi, Qp, x_vceraj))) # dodamo
-                        #skupni dobiček in parametre med kandidate
+                        kandidati.append((di, (d_strategije, Qi, Qp, x_vceraj))) # dodamo skupni dobiček, dnevni dobiček in parametre med kandidate
             M, strategija = max(kandidati) # dobimo maksimalni skupni dobiček
             d[i+1, x] = M # shranimo maksimalni dobiček
             p[i+1, x] = strategija # shranimo parametre za maksimalni dobiček
     koncna_zaloga = 0
     resitev = {}
-    for i in range(dni, 0, -1):
+    for i in range(dni, 0, -1): #izberemo optimalno strategijo za dobiček
         resitev[i, koncna_zaloga]=p[i, koncna_zaloga]
         koncna_zaloga = p[i, koncna_zaloga][3]
     resitev = dict(list(resitev.items())[::-1])
-    return (kandidati, max(kandidati)) # optimalni dobiček in parametri za rekonstrukcijo, d[dni, 0], resitev
+    return (d[dni, 0], resitev) # optimalni dobiček in parametri za rekonstrukcijo, d[dni, 0], resitev
 
